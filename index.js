22695ce7b7e17c04d9fba1b35744e776a6e9cb1a
Jenaro Wachira <wjenaro@gmail.com>
2023-08-04 17:07:38 +0300

    styling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get('', (req, res)=>{
     res.render('index.ejs');
