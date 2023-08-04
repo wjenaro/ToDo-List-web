5ff6f5cbe28b8c866c49a1513d9c26121f417b1e
Jenaro Wachira <wjenaro@gmail.com>
2023-08-04 17:51:53 +0300

    styling

--- index.js
+++ index.js
@@ -5,24 +5,42 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
+// add styling
 app.use(express.static("public"));
+// list objects to include in the list
+const taskList = [
+  "Wake up",
+  "Eat breakfast",
+  "Get dressed",
+  "Go to work",
+  "Take a break",
+  "Eat lunch",
+  "Work on projects",
+  "Take another break",
+  "Finish work",
+  "Go home",
+  "Make dinner",
+  "Eat dinner",
+  "Relax",
+  "Go to bed",
+];
 
-app.get('', (req, res)=>{
-    res.render('index.ejs');
+app.get("/", (req, res) => {
+  // Render the EJS template with the task list
+  res.render("index.ejs", { tasks: taskList });
 });
 
 // Define a route for the `/add` path
 app.post("/add", (req, res) => {
   // Get the new todo item from the request body
-  const newTodo = req.body.todo;
+  const newTask = req.body.task;
 
-  // Add the new todo item to the array
-  todos.push(newTodo);
+  // Add the new todo item to the task list
+  taskList.push(newTask);
 
   // Redirect to the todo list page
   res.redirect("/");
 });
-
 
 // Listening to the server
 app.listen(port, () => {
