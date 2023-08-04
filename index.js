840220b6278210aa8ecd68a0eee1081b380d16e3
Jenaro Wachira <wjenaro@gmail.com>
2023-08-04 18:59:00 +0300

    aug 4

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
+import ejs from "ejs";
+
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.set("view engine", "ejs");
 
 app.get('', (req, res)=>{
-    res.send("Good work");
+    res.render('index.ejs');
+});
+
+// Define a route for the `/add` path
+app.post("/add", (req, res) => {
+  // Get the new todo item from the request body
+  const newTodo = req.body.todo;
+
+  // Add the new todo item to the array
+  todos.push(newTodo);
+
+  // Redirect to the todo list page
+  res.redirect("/");
 });
 
 
